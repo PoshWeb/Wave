4a1f7afde90120487fdf030b9f94c5250d2ab7d7
James Brundage <+@users.noreply.github.com>
2026-09-13 16:34:07 -0700

    feat: Wave.NoteFrequency ( Fixes #23 )
    Normalizing notation (C#4 rather than C4#)

--- Types/Wave/get_NoteFrequency.ps1
+++ Types/Wave/get_NoteFrequency.ps1
@@ -17,19 +17,19 @@ if ($this.'#NoteFrequency') {
 # We hard code the first octave and below
 $noteFrequency = [Ordered]@{
     A0 = 27.5
-    "A0#" = 29.13523509488062
+    "A#0" = 29.13523509488062
     B0 = 30.867706328507754
     C1 = 32.70319566257483
-    "C1#" = 34.64782887210901
+    "C#0" = 34.64782887210901
     D1 = 36.70809598967595
-    "D1#" = 38.89087296526011
+    "D#0" = 38.89087296526011
     E1 = 41.20344461410874
     F1 = 43.65352892912549
-    "F1#" = 46.2493028389543
+    "F#1" = 46.2493028389543
     "G1" = 48.99942949771866
-    "G1#" = 51.91308719749314
+    "G#1" = 51.91308719749314
     "A1" = 55
-    "A1#" = 58.27047018976124
+    "A#1" = 58.27047018976124
     "B1" = 61.73541265701551
 }
 
@@ -48,6 +48,8 @@ for ($octave = 2; $octave -le $MaxOctave; $octave++) {
 
 # Add one more note for the final C
 $noteFrequency["C$($MaxOctave + 1)"] = $noteFrequency."C$($MaxOctave)" * 2
+
+# One last thing to do is transpose the keys.
 
 $this | Add-Member NoteProperty '#NoteFrequency' $noteFrequency -Force
 
