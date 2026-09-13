f7684b4cf8aba4eced3accd26c44412d037133cb
James Brundage <+@users.noreply.github.com>
2026-09-13 14:20:07 -0700

    feat: Wave.set_Data ( Fixes #18 )

---
+++ Types/Wave/set_Data.ps1
@@ -0,0 +1,46 @@
+<#
+.SYNOPSIS
+    Sets Wave Data
+.DESCRIPTION
+    Sets the PCM data in a wave.
+.LINK
+    https://en.wikipedia.org/wiki/Pulse-code_modulation
+#>
+param(
+# The Pulse Code Modulation wave data.
+[byte[]]$data
+)
+
+if (-not ($this.CanSeek -and $this.CanWrite)) {
+    throw "Must be able to seek and write a stream to change the wave"
+}
+
+# If we have no data start
+if (-not $this.'#data.start') {
+    # read chunks
+    $null = $this.Chunk
+    # If we still have no data start, return.
+    if (-not $this.'#data.start') {
+        return
+    }
+}
+
+# Seek to our data start plus 4
+$this.Position = $this.'#data.start' + 4
+# and prepare a writer
+$writer = [IO.BinaryWriter]::new($this)
+# Write our new data length
+$writer.Write([uint32]$data.Length)
+# then write our data
+$writer.Write($data)
+
+# Then update our data chunk.
+$this.'#data' = $data
+
+# and seek to near the start of the file.
+$this.Position = 4
+# Then write our stream length, minus 8 bytes
+$writer.Write([uint32]($this.Length - 8))
+
+# and set our position back to the start.
+$this.Position = 0
