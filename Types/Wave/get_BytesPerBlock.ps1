a7603615629e9bd5a8897bbee80f5d2792bfcdf6
James Brundage <+@users.noreply.github.com>
2026-09-13 13:45:38 -0700

    feat: Wave.BytesPerBlock ( Fixes #9 )

---
+++ Types/Wave/get_BytesPerBlock.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+    Gets Wave Bytes Per Block
+.DESCRIPTION
+    Gets the Wave Bytes Per Block.
+    
+    This should be the Bits Per Sample * Channel Count.
+.NOTES
+    This is the second to last pair of bytes in the Format `fmt ` chunk (`12,13`)
+#>
+
+# The expected range
+$range = 12,13
+# If we have no cached format
+if (-not $this.'#fmt') {
+    # read chunks.
+    $null = $this.Chunk
+    # Return if we still have no cached format
+    if (-not $this.'#fmt') { return }
+}
+
+# Otherwise, convert the range into a [uint16]
+[bitConverter]::ToUint16($this.'#fmt'[$range] -as [byte[]])
