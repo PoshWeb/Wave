0d69ade44efc08336f6b93ee2dbab31ce2253093
James Brundage <+@users.noreply.github.com>
2026-09-14 12:47:53 -0700

    feat: Wave.Sweep ( Fixes #25 )

---
+++ Types/Wave/Sweep.ps1
@@ -0,0 +1,98 @@
+<#
+.SYNOPSIS
+    Wave Sweep
+.DESCRIPTION
+    Generates a Wave that sweeps between two frequencies.
+#>
+param(
+# The frequency
+[Alias('Hz')]
+[float]$Frequency = 0,
+
+[Alias('Hz2')]
+[float]$ToFrequency = 0,
+
+# The duration to generate.
+[Timespan]$Duration = $(
+    if ($this.BPM -is [TimeSpan]) {$this.BPM} 
+    else { [TimeSpan]::FromSeconds(60/128) }
+),
+
+# The volume
+[float]$Volume = 0.5
+)
+
+if ($Frequency -eq 0) {
+    $Frequency = 440
+}
+
+if ($ToFrequency -eq 0) {
+    $ToFrequency = $Frequency / 2
+}
+
+# Cache our property values, so we are not wasting cycles.
+$sampleRate = $this.SampleRate
+$channelCount = $this.ChannelCount
+$BytesPerSecond = $this.BytesPerSecond
+$BitsPerSample = $this.BitsPerSample
+
+# Cache our references, for the minor speed boost it may give us.
+$math = [Math]
+$BitConverter = [BitConverter]
+
+# Calculate the number of samples
+$numberOfSamples = $math::Round($Duration.TotalSeconds * $BytesPerSecond) 
+
+# Our step size is the bits per sample / 8
+$step = $BitsPerSample/8
+
+
+# The divisor will remain the same, so compute it now.
+$divisor = ($sampleRate * $channelCount * $step)
+
+# Generate one sample at a time.
+for ($i = 0; $i -lt $numberOfSamples; $i+=$step) {
+    
+    $t = $i/$numberOfSamples
+    # Calculate the envelope
+    $envelope = 1.0 - $t
+
+    $currentFrequency = $Frequency + ($ToFrequency - $Frequency) * $t
+
+    # We can imagine each cycle as a series of circles
+    # how many circles?  Whatever our frequency may be.
+    $cycle = 2 * $math::PI * $currentFrequency
+    
+    # Calculate the angle at this point in time (in radians)
+    $angle = ($cycle * $i) / $divisor    
+
+    # The sample at this moment is the sine of that angle
+    $sample = $math::Sinh($math::Sin($angle))
+
+    # We will scale this by the volume, and then by the envelope.
+    $sample = $sample * $Volume * $envelope
+
+    # If there are 8 bits per sample
+    if ($BitsPerSample -eq 8) {
+        # round each sample into bytes, with 128 as the zero point.
+        [byte]$math::Round(            
+            128 + $sample * 127
+        )    
+    }
+
+    # If there are 16 bits per sample
+    elseif ($BitsPerSample -eq 16) {
+        # we just need to scale an `[int16]`
+        $BitConverter::GetBytes([int16]($sample * [int16]::MaxValue))
+    }
+
+    # If there are 32 bits per sample
+    elseif ($BitsPerSample -eq 32) {
+        # we can just scale to an `[int32]`
+        $BitConverter::GetBytes([int32]($sample * [int32]::MaxValue))
+    }
+}
+
+
+
+return
