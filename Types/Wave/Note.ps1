b2a48e5d933fbab8822942bed5322930d1a6cbf5
James Brundage <+@users.noreply.github.com>
2026-09-16 16:17:31 -0700

    feat: Wave.Note ( Fixes #39 )
    Expanding Capabilities.  Caching notes where possible

--- Types/Wave/Note.ps1
+++ Types/Wave/Note.ps1
@@ -1,103 +1,58 @@
 <#
 .SYNOPSIS
-    DECPS Wave Generator
+    Note Generator
 .DESCRIPTION
-    DECPS is an ancient console audio format that encodes audio in escape sequences.
-
-    Each contains:
-    
-    * An Escape Character (`[char]27`)
-    * An open bracket
-    * A volume (0-7)
-    * A fraction (32nds of a second)
-    * A series of notes in a two octave range (between C5 and C7), separated by `;`
-    * A closing ~
-    
-    For example: 
-
-    ~~~PowerShell
-    "`e[3;2;1;2;3~" # Volume of 3, Fraction of 2, notes 1, 2, 3 (C5, C#5, D5)
-    ~~~
-    
-    This converts DECPS notes into a wave form.
+    Generates a sequence of notes.    
 .NOTES
-    DECPS is a format originally supported by the [DEC VT250](https://en.wikipedia.org/wiki/VT520).
     
-    It is also supported in some moderns Terminal (Windows Terminal, Contour)
-
-    Because DECPS dates from a time where computers could only play higher frequencies,
-    this also shifts the octave when converting notes.
-    
-    By default, this shifts notes down by two octaves, into a more "normal" C3-C5 range.
-.LINK
-    https://web.mit.edu/dosathena/doc/www/ek-vt520-rm.pdf
-.LINK
-    https://tightloop.io/terminal-decps/   
-.EXAMPLE    
-    $tune = "`e[3;2;1;3;5,~`e[3;8;6;10;8;11,~`e[3;4;10;11;13;10,~`e[3;8;8;11,~`e[3;4;10;11;13;10;11;13;15;17,~`e[3;8;18;17;18,~"
-    $wav = wav DECPS $tune
-    $wav.Play()
 .EXAMPLE
-    $shortMarch = @(
-        "`e[3;20;7;7;7,~"
-        "`e[3;15;3,~"
-        "`e[3;5;10,~"        
-        "`e[3;20;7,~"
-        "`e[3;15;3,~"
-        "`e[3;5;10,~"
-        "`e[3;40;7,~"
-    ) -join [Environment]::Newline
-    wave DECPS $shortMarch play
+    wave note cafe
 .EXAMPLE
-    $closeWave = @(
-        "`e[3;10;15;17;13;2;8,~"
+    wave note d e c c3 g3
+.EXAMPLE
+    $Korobeiniki = wav @(
+        'note'
+        ('
+            e5 ++ b4 c5 -- d5 ++ c5 b4 --
+            a4 ++ a4 c5 -- e5 ++ d5 c5 --
+            b4 ++ ~ c5 -- d5 e5 c5 a4 a4 ~
+            ++ ~ d5 ~ f5 -- a5 ++ g5 f5 --
+            e5 ++ ~ c5 -- e5 ++ d5 c5 --
+            b4 ++ b4 c5 -- d5 e5 c5 a4 a4 ~
+        ' * 4)
+        '<sine> <square> <triangle> <saw>'    
     )
-    wave DECPS "`e[3;12;15;17;13;2;8,~" play
-.EXAMPLE
-    $escape = [char]27
-    $march = @(
-        "$escape[3;20;7;7;7,~",
-        "$escape[3;15;3,~",
-        "$escape[3;5;10,~",        
-        "$escape[3;20;7,~",
-        "$escape[3;15;3,~",
-        "$escape[3;5;10,~",
-        "$escape[3;40;7,~",
-        "$escape[3;20;14;14;14,~",
-        "$escape[3;15;15,~",
-        "$escape[3;5;10,~",
-        "$escape[3;20;6,~",
-        "$escape[3;15;3,~",
-        "$escape[3;5;10,~",
-        "$escape[3;40;7,~",
-        "$escape[3;20;19;7;19,~",
-        "$escape[3;10;18;17,~",
-        "$escape[3;5;16;15,~",
-        "$escape[3;10;16;0;8,~",
-        "$escape[3;20;13,~",
-        "$escape[3;10;12;11,~",
-        "$escape[3;5;10;9,~",
-        "$escape[3;10;10;0;3,~",
-        "$escape[3;20;6,~",
-        "$escape[3;15;3,~",
-        "$escape[3;5;10,~",
-        "$escape[3;20;7,~",
-        "$escape[3;15;3,~",
-        "$escape[3;5;10,~",
-        "$escape[3;40;7,~"
-    ) -join [Environment]::Newline
-    
-    $wav = wave $march
-    $wav.play()
-.EXAMPLE
+    $Korobeiniki.Play()
 
+.EXAMPLE
+    $Korobeiniki = wav @(
+        'note'
+        ('
+            𝅝 e5 𝅗𝅥 b4 c5 𝅝 d5 𝅗𝅥 c5 b4
+            𝅝 a4 𝅗𝅥 a4 c5 𝅝 e5 𝅗𝅥 d5 c5
+            𝅝 b4 𝅗𝅥 ~  c5 𝅝 d5 e5 c5 a4 a4 ~
+            𝅗𝅥 ~ d5 ~ f5 𝅝 a5 𝅗𝅥 g5 f5 
+            𝅝 e5 𝅗𝅥 ~ c5 𝅝 e5 𝅗𝅥 d5 c5 
+            𝅝 b4 𝅗𝅥 b4 c5 𝅝 d5 e5 c5 a4 a4 ~
+        ' * 4)
+        '<sine> <square> <triangle> <saw>'    
+    )
+    $Korobeiniki.Play()
+.EXAMPLE
+    $tune = 'c'
+    wave note "𝅝 $tune 𝅗𝅥 $($tune) 𝅘𝅥 $($tune * 2) 𝅘𝅥𝅮 $($tune * 4) 𝅘𝅥𝅯 $($tune * 8) 𝅘𝅥𝅰 $($tune * 16)"  play
+.EXAMPLE
+    $tune = 'cafe'
+    wave note "𝅝 $tune 𝅗𝅥 $tune 𝅘𝅥 $tune 𝅘𝅥𝅮 $tune 𝅘𝅥𝅯 $tune 𝅘𝅥𝅰 $tune 𝅘𝅥𝅲 $tune 𝅘𝅥𝅲 $tune"  play
 #>
 param(
-# The note sequence
+# The note sequence.
 [string]
 $Sequence,
 
-# The instrument or instruments used to play
+# The instrument or instruments used to play.
+# Multiple instruments may be specified.
+# Using multiple instruments will switch between each instruments
 [string]
 $Instrument = 'Tone',
 
@@ -123,12 +78,29 @@ We will approach this in three phases:
 
 #>
 
+$notemoji = "𝅝","𝅗𝅥","𝅘𝅥","𝅘𝅥𝅮","𝅘𝅥𝅯","𝅘𝅥𝅰","𝅘𝅥𝅲","𝅘𝅥𝅲"
+
+# Note: this technically matches any emoji.
+# We _could_ extend this with more emoji in the future.
+$notemojiPattern = "[\p{IsHighSurrogates}\p{IsLowSurrogates}\p{IsVariationSelectors}\p{IsCombiningHalfMarks}]+"    
+
 $notePattern = "(?>$(
     @(  
+        # Midi must come before note, otherwise `d` will match a note
         '(?<midi>m(?:idi)?\s{0,}[\d\.]+)'  
-        '(?<note>[a-g]\#?[1-8]?)'        
+        # Rest/silence/~ comes next, for almost the same reason.
+        '(?<rest>~|rest|silence)'
+        # friendly note format
+        '(?<note>[a-g]\#?[1-8]?)'
+        # A specific frequency
         '(?<frequency>[\d\.]+)'
-        '(?<rest>~)'
+        # Two pluses represent a shift uptempo.
+        # This will increase the note divisor
+        '(?<upTempo>\+\+)'
+        # Two minus represent a shift downtempt
+        # This will decrease the note divisor
+        '(?<downTempo>--)'
+        "(?<notemoji>$notemojiPattern)"
     ) -join '|'
 ))"
 
@@ -149,39 +121,93 @@ $progress = @{
 # (We may as well log what happens, as it gives us history and readable note sequences)
 $events = [Runspace]::DefaultRunspace.Events
 
+# Set our base time from our duration
+$BaseTime = [TimeSpan]::FromSeconds($Duration.TotalSeconds)
+# and start the divisor at one.
+$divisor = 1
+
 $NoteSequence = @(
-    # Walk over our array of DECPS notes
-    for ($index = 0; $index -lt $noteMatches.Length; $index++) {        
-        $match = $noteMatches[$index]
+    # Walk over our array of notes
+    for ($index = 0; $index -lt $noteMatches.Length; $index++) {
+        # If the divisor was positive
+        if ($divisor -gt 1) {
+            # our tempo at the moment is the base time divided by our divisor
+            $Tempo = [TimeSpan]::FromSeconds($BaseTime.TotalSeconds / $divisor)
+        } 
+        elseif ($divisor -le -2) {
+            $tempo = [TimeSpan]::FromSeconds($BaseTime.TotalSeconds * ([Math]::Abs($divisor)))
+        }
+        else {
+            # otherwise it is the base time.
+            $Tempo = [TimeSpan]::FromSeconds($BaseTime.TotalSeconds)
+        }
+
+        # Get our match
+        $match = $noteMatches[$index]        
+
+        # If our match was a midi note
         if ($match.Groups['midi'].Success) {
-            $midiNote = $match -replace '\p{L}' -as [double]
-            $frequency = $this.MidiNoteToFrequency($midiNote)
+            # replace any letters and whitespace and cast to a double
+            $midiNote = $match -replace '[\p{L}\s]' -as [double]
+            # then convert it to a frequency.
+            $frequency = 440.0 * [Math]::Pow(
+                2, (($MidiNote - 69)/12)
+            )
+            # Then output the note
             [Ordered]@{
                 Name = "$match"; Frequency = $frequency
-                Duration = $Duration; Volume = $volume
+                Duration = $Tempo; Volume = $volume
             }
         }
+        # If our match was a friendly note
         elseif ($match.Groups['note'].Success) {
+            # get it's name
             $friendly = "$match"
-            if ($friendly -notmatch '\d$') {
+            # if it did not have a digit
+            if ($friendly -notmatch '[a-g]\#?\d$') {
+                # add one
                 $friendly += 4
             }
+            # Output our note
             [Ordered]@{
                 Name = $friendly; Frequency = $noteFrequency[$friendly]
-                Duration = $Duration; Volume = $volume
+                Duration = $Tempo; Volume = $volume
             }   
         }
         elseif ($match.Groups['frequency'].Success) {
+            # If we were provided a specific frequency
+            # that is our note.
             [Ordered]@{
                 Name = "${match}hz"; Frequency = "$match" -as [float]
-                Duration = $Duration; Volume = $volume
+                Duration = $Tempo; Volume = $volume
             }
         }
         elseif ($match.Groups['rest'].Success) {
+            # If we were provided a rest, our note is no note.
             [Ordered]@{
                 Name = "Rest"; Frequency = 0
-                Duration = $Duration; Volume = $volume
+                Duration = $Tempo; Volume = $volume
             }
+        }
+        elseif ($match.Groups['upTempo'].Success) {
+            # If we are going uptempo, increase the divisor
+            $divisor++            
+            if ($divisor -eq -1) {
+                $divisor = 1
+            }
+        }
+        elseif ($match.Groups['downTempo'].Success) {
+            # If we are going downtemp, decrease the divisor
+            $divisor--
+            if ($divisor -eq 0) {
+                $divisor = -2
+            }
+        }
+        elseif ($match.Groups['notemoji'].Success) {
+            $notemojiIndex = $notemoji.IndexOf("$match")
+            if ($notemojiIndex -ge 0) {
+                $divisor = $notemojiIndex + 1
+            }            
         }
     
         # writing progress as we go
@@ -209,14 +235,32 @@ $null = $events.GenerateEvent(
 
 # Replace angle brackets 
 # and split on whitespace to get a sequence of instruments
-$Instruments = @($Instrument -replace '[<>]') -split '\s+'
+$Instruments = @($Instrument -replace '[\p{P}<>]') -split '\s+'
 # Start at the first instrument.
 $InstrumentIndex = 0 
 
 # Update our progress info
 $progress.status = 'Generating'
 
-# And make waves!
+# We never need to make the same wave twice,
+# so we want a runspace-wide cache of our waves.
+# This makes it _much_ faster to generate most notes
+$waveTable = Get-TypeData -TypeName WaveTable -ErrorAction Ignore
+
+# If the cache is uninitialized
+if (-not $waveTable.Members.WaveCache) {
+    # Create it
+    Update-TypeData -TypeName WaveTable -MemberName "WaveCache" -MemberType NoteProperty -Value (
+        [Ordered]@{}
+    ) -Force
+}
+
+# Create a Wave Table prototype
+$waveTable = [PSCustomObject]@{PSTypeName='WaveTable'}
+# and get the static cache.
+$waveCache = $waveTable.WaveCache
+
+# Now Let's make some make waves!
 
 # We go over each note in the sequence,
 for ($index = 0; $index -lt $NoteSequence.Length; $index++) {
@@ -227,26 +271,26 @@ for ($index = 0; $index -lt $NoteSequence.Length; $index++) {
     $progress.Activity = "$($note.Name)@$($note.Duration) $index / $($NoteSequence.Length)"
     Write-Progress @progress    
 
-
-    if ($note.Name -eq 'Rest') {
-
-    }
-    
-
     # Get the instrument used for this sample.
     if ($Instruments.Length) {
         $Instrument = $Instruments[$InstrumentIndex % $Instruments.Length]
         $InstrumentIndex++
     }
     
+    # If the note name is rest or the frequency is zero
     if (-not $note.Frequency -or $Name -eq 'REST') { 
+        # the instrument is `Silence`.
         $Instrument = 'Silence'        
+    }
+
+    if (-not $this.Instrument.Script) {
+        throw "Invalid Instrument $Instrument (must be a script method)"
     }
 
     # Then find our instrument parameter names
     $instrumentParameterNames =
         # by walking over the parameters in the script's AST
-        foreach ($param in $this.$Instrument.Script.Ast.ParamBlock.Parameters) { 
+        foreach ($param in $this.$Instrument.Script.Ast.ParamBlock.Parameters) {
             "$($param.Name)" -replace '^\$'
             foreach ($attr in $param.Attributes) {
                 if ($attr.TypeName.Name -eq 'alias') {
@@ -255,30 +299,55 @@ for ($index = 0; $index -lt $NoteSequence.Length; $index++) {
             }
         }
 
+    if (-not $instrumentParameterNames.Count) {
+        throw "No Instrument Parameters for $instrument"
+    }
+
     # Collect our instrument parameters
     $instrumentParameters = [Ordered]@{}
     
-    # Any information in our note
-    foreach ($key in $note.Keys) {
-        # that is a parameter for that instrument
-        if ($instrumentParameterNames -contains $key) {
-            # becomes an instrument parameter.
-            $instrumentParameters[$key] = $note[$key]
+    # As we map our instrument parameters, let us also construct a cache key.
+    # If the same instrument and parameters are requested,
+    # we can reliably avoid making the same note twice.
+    
+    # We might as well keep this key in a html friendly format.
+    # (we might want this information later)
+    $cacheKey = @(
+        "data-audio-format='$($this.AudioFormat)'"
+        "data-channel-count='$($this.ChannelCount)'"
+        "data-sample-rate='$($this.SampleRate)'"
+        "data-bits-per-sample='$($this.BitsPerSample)'"
+        "data-instrument='$($Instrument -replace "'", "''")'"
+        foreach ($key in $note.Keys) {
+            "data-$key='$($note[$key] -replace "'","''")'"
+            # that is a parameter for that instrument
+            if ($instrumentParameterNames -contains $key) {
+                # becomes an instrument parameter.
+                $instrumentParameters[$key] = $note[$key]
+            }
         }
-    }
+    ) -join ' '
         
-    # Generate an event representing a note 
+    # Generate an event representing a note.
+    # Even if we have already cached the note, 
+    # we still want to log that we want to play it.
     $null = $events.GenerateEvent(
         'Note', $this, @($friendly), (
             [Ordered]@{} + $note + @{
                 # played with the current instrument
                 Instrument = $Instrument
+                Id = $cacheKey 
             }
         )
     )
+    
+    if (-not $WaveCache[$cacheKey]) {
+        # And call our instrument script.  This should output a stream of bytes.
+        $WaveCache[$cacheKey] = [byte[]](. $this.$instrument.Script @instrumentParameters)
+    }
 
-    # And call our instrument script.  This should output a stream of bytes.
-    & $this.$instrument.Script @instrumentParameters
+    $WaveCache[$cacheKey]    
+    # . $this.$instrument.Script @instrumentParameters
 }    
 
 # Last but not least, complete our progress bars.
