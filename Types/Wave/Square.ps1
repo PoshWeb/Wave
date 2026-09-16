5248077afd6ada30a9c8072a4303e27b5f378862
James Brundage <+@users.noreply.github.com>
2026-09-15 17:15:08 -0700

    feat: Wave.Square ( Fixes #21 )
    Making the wave square, defaulting volume from `$this`

--- Types/Wave/Square.ps1
+++ Types/Wave/Square.ps1
@@ -1,14 +1,20 @@
 <#
 .SYNOPSIS
-    Square Tone Generator
+    Square tone Generator
 .DESCRIPTION
-    Generates a square tone of a `-Frequency`, for `-Time`, at `-Volume`.
+    Generates a square tone of a `-Frequency`, for `-Time`, at `-Volume`
 .NOTES
-    This is the same a pure tone, 
-    using `[Math]::Sinh` to round values to 1 or -1
+    A square wave is calculated by taking the sin of a given angle and using:
+
+    ~~~PowerShell
+    [Math]::atan([Math]::Tan($angle/2))
+    ~~~
+.LINK
+    https://en.wikipedia.org/wiki/Square_wave_(waveform)
 #>
 param(
-# The frequency
+# The frequency.
+# Defaults to A4 (440hz)
 [Alias('Hz')]
 [float]$Frequency = 440,
 
@@ -19,7 +25,10 @@ param(
 ),
 
 # The volume
-[float]$Volume = 0.5
+[float]$Volume = $(
+    if ($this.Volume) { $this.Volume }
+    else { 0.5 }
+)
 )
 
 # Cache our property values, so we are not wasting cycles.
@@ -31,6 +40,7 @@ $BitsPerSample = $this.BitsPerSample
 # Cache our references, for the minor speed boost it may give us.
 $math = [Math]
 $BitConverter = [BitConverter]
+$GetBytes = $BitConverter::GetBytes
 
 # Calculate the number of samples
 $numberOfSamples = $math::Round($Duration.TotalSeconds * $BytesPerSecond) 
@@ -45,24 +55,32 @@ $cycle = 2 * $math::PI * $Frequency
 # The divisor will remain the same, so compute it now.
 $divisor = ($sampleRate * $channelCount * $step)
 
-
-
-
 # Generate one sample at a time.
 for ($i = 0; $i -lt $numberOfSamples; $i+=$step) {
-    # Normally, we would calculate the envelope.
-    # But a square wave does not have an envelope.
-    # (or rather, is has an envelope of 1 )
-    # $envelope = 1.0    
+
+    # Calculate the envelope
+    $envelope = 1.0 - ($i / $numberOfSamples)
+
+    # We can imagine each cycle as a series of circles
+    # how many circles?  Whatever our frequency may be.
+    $cycle = (2 * $math::PI * $Frequency)
     
     # Calculate the angle at this point in time (in radians)
     $angle = ($cycle * $i) / $divisor    
+    
+    # Calculate the angle at the moment
+    $sample = $math::sin($angle)
+    
+    # For a square wave, 
+    # we simply check the sign of the sample.
+    if ($sample -gt 0) {
+        $sample = 1 # and use either 1
+    } elseif ($sample -lt 0) {
+        $sample = -1 # or negative 1.
+    }
 
-    # The sample at this moment is the sine of that angle
-    $sample = $math::Sin($angle)
-
-    # We will scale this by the volume
-    $sample = $sample * $Volume
+    # We will scale this by the volume, and then by the envelope.
+    $sample = $sample * $volume * $envelope
 
     # Clamp our sample
     $sample = $math::Clamp($sample, -1.0, 1.0)
@@ -88,14 +106,16 @@ for ($i = 0; $i -lt $numberOfSamples; $i+=$step) {
     elseif ($BitsPerSample -eq 16) {
         # we just need to scale an `[int16]`
         # Hardcode `[int16]::MaxValue` for speed
-        $BitConverter::GetBytes([int16]($sample * 32767))
+        $GetBytes.Invoke([int16]($sample * 32767))
     }
 
     # If there are 32 bits per sample
     elseif ($BitsPerSample -eq 32) {
         # we can just scale to an `[int32]`.
         # Hardcode `[int32]::MaxValue` for speed
-        $BitConverter::GetBytes([int32]($sample * 2147483647))
+        $GetBytes.Invoke([int32]($sample * 2147483647))
     }
     #endregion Encode Sample
 }
+
+return
