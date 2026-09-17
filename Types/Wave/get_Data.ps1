20e02ff0abc237c2a29100a5b0a282b40eeac48b
James Brundage <+@users.noreply.github.com>
2026-09-17 15:06:20 -0700

    feat: Wave.Data ( Fixes #6 )
    Initializing data if unread

--- Types/Wave/get_Data.ps1
+++ Types/Wave/get_Data.ps1
@@ -6,6 +6,16 @@
 #>
 [CmdletBinding()]
 param()
+
+# If we have not cached our data chunk
+if (-not $this.'#data') {
+    # read chunks
+    $null = $this.Chunk
+    # If there is still nothing
+    if (-not $this.'#data') {
+        return # return nothing.
+    }
+}
 # Use the comma operator to return a list containin the data, 
 # so that it unrolls to the data itself (rather than tries to stream the bytes)
 return ,$this.'#data'
