118ba66f778b2949da9d8a04846ae869957ddf93
James Brundage <+@users.noreply.github.com>
2026-09-17 14:23:04 -0700

    feat: Wave.Sweep ( Fixes #25 )
    Returning [double[]]

--- Types/Wave/Sweep.ps1
+++ Types/Wave/Sweep.ps1
@@ -4,6 +4,7 @@
 .DESCRIPTION
     Generates a Wave that sweeps between two frequencies.    
 #>
+[OutputType([double[]])]
 param(
 # The frequency.
 # If no frequency is provided, it will be 440hz (A4)
@@ -49,14 +50,6 @@ param(
 [uint16]$channelCount = $(
     if ($this.ChannelCount) { $this.ChannelCount } 
     else { 1 }
-),
-
-# The audio format.
-# Will default to the `.AudioFormat` of `$this` wave.
-# If there is no `$this` wave, will default to 3 (IEEE floating point).
-[uint16]$AudioFormat = $(
-    if ($this.AudioFormat) { $this.AudioFormat } 
-    else { 3 }
 )
 )
 
@@ -80,77 +73,29 @@ $step = $BitsPerSample/8
 # The divisor will remain the same, so compute it now.
 $divisor = ($sampleRate * $channelCount * $step)
 
-# Generate one sample at a time.
-for ($i = 0; $i -lt $numberOfSamples; $i+=$step) {
+# We will return the wave as a `[double[]]`,
+# and preceeed it by a comma so that we return all samples at once.
+return ,[double[]]@(
+    # Generate one sample at a time.
+    for ($i = 0; $i -lt $numberOfSamples; $i+=$step) {
     
-    $t = $i/$numberOfSamples
-    # Calculate the envelope
-    $envelope = 1.0 - $t
+        $t = $i/$numberOfSamples
+        # Calculate the envelope
+        $envelope = 1.0 - $t
 
-    $currentFrequency = $Frequency + ($ToFrequency - $Frequency) * $t
+        $currentFrequency = $Frequency + ($ToFrequency - $Frequency) * $t
 
-    # We can imagine each cycle as a series of circles
-    # how many circles?  Whatever our frequency may be.
-    $cycle = 2 * $math::PI * $currentFrequency
-    
-    # Calculate the angle at this point in time (in radians)
-    $angle = ($cycle * $i) / $divisor    
+        # We can imagine each cycle as a series of circles
+        # how many circles?  Whatever our frequency may be.
+        $cycle = 2 * $math::PI * $currentFrequency
+        
+        # Calculate the angle at this point in time (in radians)
+        $angle = ($cycle * $i) / $divisor    
 
-    # The sample at this moment is the sine of that angle
-    $sample = $math::Sin($angle)    
+        # The sample at this moment is the sine of that angle
+        $sample = $math::Sin($angle)    
 
-    # We will scale this by the volume, and then by the envelope.
-    $sample = $sample * $Volume * $envelope
-
-    #region Encode Sample
-
-    # We _could_ encapsulate the encoding off into it's own procedure.
-
-    # However, callstacks have overhead.
-
-    # Inline code will be quicker.
-    # (hence duplicating it across multiple files)
-
-    # If we are using 32-bit floating point audio
-    if ($BitsPerSample -eq 32 -and $audioFormat -eq 3) {
-        # we are basically done.
-        # No clamping required. # Just cast to float, 
-        $GetBytes.Invoke([float]$sample) # get the bytes,
-        continue # and continue 
+        # We will scale this by the volume, and then by the envelope.
+        $sample * $Volume * $envelope
     }
-    
-    # If we are dealing with [byte], [int16], or [int32] audio formats,
-    # We've got to clamp it down to an amplitude between -1 and 1.    
-
-    # Unfortunately, `Clamp` is not part of older .NET framework versions
-    # So we will clamp the old-fashioned way, with an `if`
-    if ($sample -gt 1) { $sample = 1 }
-    elseif ($sample -lt -1) { $sample = -1 }
-
-    # If there are 8 bits per sample
-    if ($BitsPerSample -eq 8) {        
-        # round each sample into bytes, with 128 as the zero point.
-        [byte]$math::Round(
-            128 + $sample * 127
-        )
-    }
-
-    # If there are 16 bits per sample
-    elseif ($BitsPerSample -eq 16) {
-        # we just need to scale an `[int16]`
-        # Hardcode `[int16]::MaxValue` for speed
-        $GetBytes.Invoke([int16]($sample * 32767))
-    }
-
-    # If there are 32 bits per sample
-    elseif ($BitsPerSample -eq 32) {
-        # we can just scale to an `[int32]`.
-        # Hardcode `[int32]::MaxValue` for speed
-        $GetBytes.Invoke([int32]($sample * 2147483647))
-    }
-    #endregion Encode Sample
-}
-
-
-
-return
+)
