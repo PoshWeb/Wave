b3d4ec1ae33b419e1d844d75b9eb00105f869fd7
James Brundage <+@users.noreply.github.com>
2026-09-18 19:44:24 -0700

    docs: Wave.TimeBase ( Fixes #51 )
    Adding Help

--- Types/Wave/get_TimeBase.ps1
+++ Types/Wave/get_TimeBase.ps1
@@ -1,3 +1,17 @@
+<#
+.SYNOPSIS
+    Gets the Wave Time Base
+.DESCRIPTION
+    Gets the Wave Time Base.  
+    
+    This is the smallest amount of time that the wave records.
+.NOTES
+    Since the sample rate is the number of samples per second, 
+    
+    This is the reciprocal of that value, expressed as a `[TimeSpan]`
+#>
+param()
+
 $sampleRate = $this.SampleRate
 
 if (-not $sampleRate) { return [TimeSpan]::FromSeconds(0) }
