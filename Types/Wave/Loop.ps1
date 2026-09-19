9f53fe0841c6a789bc1cc2eae15f6425e0a387d8
James Brundage <+@users.noreply.github.com>
2026-09-19 13:48:04 -0700

    feat: Wave.Loop ( Fixes #28 )
    Returning a new wave

--- Types/Wave/Loop.ps1
+++ Types/Wave/Loop.ps1
@@ -4,26 +4,20 @@
 .DESCRIPTION
     Loops a Wave `-LoopCount` number of times.
 .NOTES
-    A loop is just a repeat of the wave data, or a copy of the array.    
+    A loop is just a repeat of the wave data, or a copy of the array.
 #>
+[OutputType('audio/wav')]
 param(
-# The Loop Count
+# The Loop Count.  If no loop count is specified, will loop twice.
 [uint16]
-$LoopCount = 1,
-
-[byte[]]
-$Data
+$LoopCount
 )
 
-if (-not $LoopCount) {
-    return
-}
+if (-not $LoopCount) { $LoopCount = 2 }
 
-if (-not $data) {
-    $data = $this.Data
-}
-
+[byte[]]$data = $this.Data
 # We can do this very easily in PowerShell,
 # as multiplying a list by an integer duplicates the list.
-# So we just `*=` our existing data.
-$Data * $LoopCount
+# So we just `*` our existing data.
+$waveFormat = $this.WaveFormat
+return wave @waveFormat -PCM ($Data * $LoopCount)
