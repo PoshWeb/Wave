b41d5a8f37e7b93cd628150393b4a71170506392
James Brundage <+@users.noreply.github.com>
2026-09-20 14:48:37 -0700

    feat: Wave.Volume ( Fixes #38 )
    Detecting volume

--- Types/Wave/get_Volume.ps1
+++ Types/Wave/get_Volume.ps1
@@ -6,8 +6,19 @@
 
     This is used to provide a default value for Wave methods that use `-Volume`.
 
-    If no Volume has been provided, returns nothing.
+    If no Volume has been provided, will measure the volume of the samples.
 #>
 if ($this.'#Volume') {
     return $this.'#Volume'
+}
+$samples = $this.Samples
+# Get our samples as a `[double[]]`
+[double[]]$samples = $this.Samples
+
+# If we had any samples
+if ($samples) {
+    # then the max volume of the entire sequence is the max minus the min.
+    return [Linq.Enumerable]::Max($samples) - [Linq.Enumerable]::Min($samples)    
+} else {
+    return 0
 }
