8bc2146e6109affbe7a4e9f037b57ac4aeea34a0
James Brundage <+@users.noreply.github.com>
2026-09-21 13:11:55 -0700

    feat: Wave.Reverse ( Fixes #50 )
    Using Linq

--- Types/Wave/Reverse.ps1
+++ Types/Wave/Reverse.ps1
@@ -13,7 +13,7 @@
 [OutputType('audio/wav')]
 param()
 
-[double[]]$samples = $this.Samples
-[Array]::Reverse($samples)
 $waveFormat = $this.WaveFormat
-return wave @waveFormat -Samples $samples
+return wave @waveFormat -Samples (
+    [Linq.Enumerable]::Reverse([double[]]$this.Samples)
+)
